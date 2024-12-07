4e46609668bc687235aa74f9c4fa5a08582873e2
felipinhobgtom <felipinhobgouveia@gmail.com>
2024-12-07 20:40:56 -0300

    footer reworked

---
+++ src/components/footer/img/Ball.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Ball() {
+  return (
+    <svg
+      width="6"
+      height="6"
+      viewBox="0 0 6 6"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="3" cy="2.66016" r="2.5" fill="#1E1D1D" />
+    </svg>
+  );
+}
+
+export default Ball;
---
+++ src/components/footer/img/SymBee.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function SymBee() {
+  return (
+    <svg
+      width="35"
+      height="46"
+      viewBox="0 0 31 46"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.0283 30.2936L15.8962 22.8513L28.7075 30.5459V23.1036L15.8962 15.9136L1.5 23.3559V15.4091L15.8962 7.9668L29.5 16.1659"
+        stroke="#F9D504"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5.99023 33.9521L12.7261 37.7134L15.436 39.2271L18.2733 37.7134L25.0091 33.9522"
+        stroke="#1E1D1D"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.878 6.07474C10.878 6.07474 9.92881 4.39318 9.02897 3.55193C8.18041 2.75863 6.51953 1.91211 6.51953 1.91211"
+        stroke="#1E1D1D"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M20.5185 5.82278C20.5185 5.82278 21.4677 4.14123 22.3675 3.29998C23.2161 2.50668 24.877 1.66016 24.877 1.66016"
+        stroke="#1E1D1D"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default SymBee;
